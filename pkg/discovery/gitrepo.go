ca92474ae3e7260de5287ce1cfa00bd4ed35aa79
Robert Kaussow <mail@thegeeklab.de>
2025-12-14 16:57:01 +0100

    fix: ensure kubernetes names sanitizing

--- pkg/discovery/gitrepo.go
+++ pkg/discovery/gitrepo.go
@@ -1,17 +1,23 @@
 package discovery
 
 import (
-	"strings"
+	"fmt"
 
 	renovatev1beta1 "github.com/thegeeklab/renovate-operator/api/v1beta1"
+	"github.com/thegeeklab/renovate-operator/pkg/util/k8s"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func CreateGitRepo(owner client.Object, namespace, repo string) *renovatev1beta1.GitRepo {
+func CreateGitRepo(owner client.Object, namespace, repo string) (*renovatev1beta1.GitRepo, error) {
+	sanitizedName, err := k8s.SanitizeName(repo)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sanitize repository name: %w", err)
+	}
+
 	gitRepo := &renovatev1beta1.GitRepo{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      sanitizeRepoName(repo),
+			Name:      sanitizedName,
 			Namespace: namespace,
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(owner, renovatev1beta1.GroupVersion.WithKind("Renovator")),
@@ -22,9 +28,5 @@ func CreateGitRepo(owner client.Object, namespace, repo string) *renovatev1beta1
 		},
 	}
 
-	return gitRepo
-}
-
-func sanitizeRepoName(repo string) string {
-	return strings.ToLower(strings.ReplaceAll(repo, "/", "-"))
+	return gitRepo, nil
 }
