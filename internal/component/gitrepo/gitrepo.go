b1c97a56caf4069f22467232025851cba45bd710
Robert Kaussow <mail@thegeeklab.de>
2026-05-20 11:34:18 +0200

    fix: trigger initial renovate run on webhook creation

--- internal/component/gitrepo/gitrepo.go
+++ internal/component/gitrepo/gitrepo.go
@@ -5,7 +5,9 @@ import (
 
 	renovatev1beta1 "github.com/thegeeklab/renovate-operator/api/v1beta1"
 	"github.com/thegeeklab/renovate-operator/pkg/util/k8s"
+	api_errors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
@@ -14,12 +16,10 @@ func (r *Reconciler) reconcileGitRepo(ctx context.Context) (*ctrl.Result, error)
 	if !r.instance.DeletionTimestamp.IsZero() {
 		if r.instance.Status.WebhookID == "" &&
 			controllerutil.ContainsFinalizer(r.instance, renovatev1beta1.FinalizerGitRepoWebhook) {
-			_, err := k8s.CreateOrUpdate(ctx, r.Client, r.instance, nil, func() error {
-				controllerutil.RemoveFinalizer(r.instance, renovatev1beta1.FinalizerGitRepoWebhook)
+			patch := client.MergeFrom(r.instance.DeepCopy())
+			controllerutil.RemoveFinalizer(r.instance, renovatev1beta1.FinalizerGitRepoWebhook)
 
-				return nil
-			})
-			if err != nil {
+			if err := r.Patch(ctx, r.instance, patch); err != nil && !api_errors.IsNotFound(err) {
 				return &ctrl.Result{}, err
 			}
 		}
