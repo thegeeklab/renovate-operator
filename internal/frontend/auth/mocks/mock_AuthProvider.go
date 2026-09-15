cdbe43c401d5bd570e00f38f58273ea32156f174
Robert Kaussow <mail@thegeeklab.de>
2026-09-15 09:04:14 +0200

    re-generate mocks

--- internal/frontend/auth/mocks/mock_AuthProvider.go
+++ internal/frontend/auth/mocks/mock_AuthProvider.go
@@ -18,10 +18,19 @@ func NewAuthProvider(t interface {
 	mock.TestingT
 	Cleanup(func())
 }) *AuthProvider {
+	if helper, ok := t.(interface{ Helper() }); ok {
+		helper.Helper()
+	}
+
 	mock := &AuthProvider{}
 	mock.Mock.Test(t)
 
-	t.Cleanup(func() { mock.AssertExpectations(t) })
+	t.Cleanup(func() {
+		if helper, ok := t.(interface{ Helper() }); ok {
+			helper.Helper()
+		}
+		mock.AssertExpectations(t)
+	})
 
 	return mock
 }
--- internal/receiver/mocks/mock_Receiver.go
+++ internal/receiver/mocks/mock_Receiver.go
@@ -17,10 +17,19 @@ func NewReceiver(t interface {
 	mock.TestingT
 	Cleanup(func())
 }) *Receiver {
+	if helper, ok := t.(interface{ Helper() }); ok {
+		helper.Helper()
+	}
+
 	mock := &Receiver{}
 	mock.Mock.Test(t)
 
-	t.Cleanup(func() { mock.AssertExpectations(t) })
+	t.Cleanup(func() {
+		if helper, ok := t.(interface{ Helper() }); ok {
+			helper.Helper()
+		}
+		mock.AssertExpectations(t)
+	})
 
 	return mock
 }
